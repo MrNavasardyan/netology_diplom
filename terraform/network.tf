1eaedd75f2a93c20d5eefd0838c8663ed04f1d4c
ngrachik <ngrachik2011@gmail.com>
2022-07-05 13:07:27 +0300

    modified:   terraform/network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -6,6 +6,6 @@ resource "yandex_vpc_network" "network" {
 resource "yandex_vpc_subnet" "subnet" {
   name = "subnet"
   zone           = "ru-central1-a"
-  network_id     = "${yandex_vpc_network.default.id}"
+  network_id     = "${yandex_vpc_network.subnet.id}"
   v4_cidr_blocks = ["192.168.101.0/24"]
 }
