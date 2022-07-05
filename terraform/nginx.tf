835476134a508092bf8f82c585372a28311b7410
ngrachik <ngrachik2011@gmail.com>
2022-07-05 13:56:45 +0300

    Changes to be committed: 	new file:   terraform/nginx.tf

---
+++ terraform/nginx.tf
@@ -0,0 +1,33 @@
+resource "yandex_compute_instance" "my-domain" {
+ count          = var.my-domain
+ name = "my-domain-${terraform.workspace}"
+
+ resources {
+       cores = "2"
+       memory = "2"
+ }
+
+ boot_disk {
+   initialize_params {
+     image_id = "fd8sgdi3n1kbkn8n5hql"
+   }
+ }
+
+   metadata = {
+   ssh-keys = "centos:${file("~/.ssh/id_rsa.pub")}"
+ }
+
+
+ network_interface {
+   subnet_id = yandex_vpc_subnet.subnet-count.id
+   nat       = true
+ }
+}
+
+output "internal_ip_address_vm_instance_db" {
+  value = yandex_compute_instance.my-domain.*.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_instance_db" {
+  value = yandex_compute_instance.my-domain.*.network_interface.0.nat_ip_address
+}
