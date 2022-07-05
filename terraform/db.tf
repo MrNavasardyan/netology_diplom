f6811b74563771627fc74c34eb9e40c957e76d64
ngrachik <ngrachik2011@gmail.com>
2022-07-05 13:13:47 +0300

    Changes to be committed: 	modified:   terraform/db.tf

--- terraform/db.tf
+++ terraform/db.tf
@@ -25,9 +25,9 @@ resource "yandex_compute_instance" "db-count" {
 }
 
 output "internal_ip_address_vm_instance_db" {
-  value = yandex_compute_instance.vm-count.*.network_interface.0.ip_address
+  value = yandex_compute_instance.db-count.*.network_interface.0.ip_address
 }
 
 output "external_ip_address_vm_instance_db" {
-  value = yandex_compute_instance.vm-count.*.network_interface.0.nat_ip_address
+  value = yandex_compute_instance.db-count.*.network_interface.0.nat_ip_address
 }
