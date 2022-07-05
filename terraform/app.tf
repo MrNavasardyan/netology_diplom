d92d290a528b5472cfb63cb0a4e7abefd5324325
ngrachik <ngrachik2011@gmail.com>
2022-07-05 14:15:54 +0300

    Changes to be committed: 	modified:   terraform/app.tf 	modified:   terraform/outputs.tf

---
+++ terraform/app.tf
@@ -0,0 +1,24 @@
+resource "yandex_compute_instance" "app" {
+ name = "app-${terraform.workspace}"
+
+ resources {
+       cores = "2"
+       memory = "2"
+ }
+
+ boot_disk {
+   initialize_params {
+     image_id = "fd8sgdi3n1kbkn8n5hql"
+   }
+ }
+
+   metadata = {
+   ssh-keys = "centos:${file("~/.ssh/id_rsa.pub")}"
+ }
+
+
+ network_interface {
+   subnet_id = yandex_vpc_subnet.subnet-count.id
+   nat       = true
+ }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,3 +14,27 @@ output "internal_ip_address_vm_instance_db" {
 output "external_ip_address_vm_instance_db" {
   value = yandex_compute_instance.db-count.*.network_interface.0.nat_ip_address
 }
+
+output "internal_ip_address_vm_instance_app" {
+  value = yandex_compute_instance.app.*.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_instance_app" {
+  value = yandex_compute_instance.app.*.network_interface.0.nat_ip_address
+}
+
+output "internal_ip_address_vm_instance_runner" {
+  value = yandex_compute_instance.runner.*.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_instance_runner" {
+  value = yandex_compute_instance.runner.*.network_interface.0.nat_ip_address
+}
+
+output "internal_ip_address_vm_instance_gitlab" {
+  value = yandex_compute_instance.gitlab.*.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_instance_gitlab" {
+  value = yandex_compute_instance.gitlab.*.network_interface.0.nat_ip_address
+}
