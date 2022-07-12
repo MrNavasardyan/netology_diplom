cb8834310d8cd49fbe773feb9cb5482bd460e2e3
ngrachik <ngrachik2011@gmail.com>
2022-07-12 14:05:08 +0300

    added bucket

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,19 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
+
+
+backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "tf-state-bucket-diplom"
+    region     = "ru-central1"
+    key        = "terraform.tfstate.d/stage/terraform.tfstate"
+    access_key = "ajejd937kgbjepe2o5ga"
+    secret_key = "YCNeV0p9LftLPNvjV-0PDH44thPB9OgQDFlNPp4q"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
 }
 
 provider "yandex" {
@@ -13,3 +26,12 @@ provider "yandex" {
   folder_id = "${var.yandex_folder_id}"
   zone = "${var.zone}"
 }
+
+
+
+# access_key:
+#   id: ajejd937kgbjepe2o5ga
+#   service_account_id: ajej7rfe81jr4jphc22m
+#   created_at: "2022-07-12T11:00:53.894700072Z"
+#   key_id: YCAJEu1pkTKYffW707qbyKlvQ
+# secret: YCNeV0p9LftLPNvjV-0PDH44thPB9OgQDFlNPp4q
