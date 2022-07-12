1d42ce2f214234b9536186788575fc3364c8b40f
ngrachik <ngrachik2011@gmail.com>
2022-07-12 14:55:32 +0300

    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   terraform/provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,20 +5,20 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
+ 
+
+backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "tf-state-bucket-nav"
+    region     = "ru-central1"
+    key        = "terraform.tfstate"
+    access_key = "YCAJEu1pkTKYffW707qbyKlvQ"
+    secret_key = "YCNeV0p9LftLPNvjV-0PDH44thPB9OgQDFlNPp4q"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
   }
-
-
-# backend "s3" {
-#     endpoint   = "storage.yandexcloud.net"
-#     bucket     = "tf-state-bucket-diplom"
-#     region     = "ru-central1"
-#     key        = "terraform.tfstate.d/stage/terraform.tfstate"
-#     access_key = "YCAJEu1pkTKYffW707qbyKlvQ"
-#     secret_key = "YCNeV0p9LftLPNvjV-0PDH44thPB9OgQDFlNPp4q"
-
-#     skip_region_validation      = true
-#     skip_credentials_validation = true
-#   }
+ }
 
 resource "yandex_storage_bucket" "state" {
   access_key = "YCAJEu1pkTKYffW707qbyKlvQ"
