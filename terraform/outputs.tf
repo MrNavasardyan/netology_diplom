1816c8e36bd79b4e4d670f7998ce2ec1519d6ef2
ngrachik <ngrachik2011@gmail.com>
2022-07-18 15:45:16 +0300

    Changes to be committed: 	modified:   terraform/outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -16,11 +16,11 @@ output "external_ip_address_vm_instance_db" {
 }
 
 output "internal_ip_address_vm_instance_app" {
-  value = yandex_compute_instance.app.grachikn.ru.*.network_interface.0.ip_address
+  value = yandex_compute_instance.app.*.network_interface.0.ip_address
 }
 
 output "external_ip_address_vm_instance_app" {
-  value = yandex_compute_instance.app.grachikn.ru.*.network_interface.0.nat_ip_address
+  value = yandex_compute_instance.app.*.network_interface.0.nat_ip_address
 }
 
 output "internal_ip_address_vm_instance_runner" {
