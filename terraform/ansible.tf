711cb6339a02c208b576ebc63313df5c27e8663e
ngrachik <ngrachik2011@gmail.com>
2022-07-18 15:59:21 +0300

    Changes to be committed: 	new file:   terraform/ansible.tf

---
+++ terraform/ansible.tf
@@ -0,0 +1,20 @@
+resource "null_resource" "wait" {
+  provisioner "local-exec" {
+    command = "sleep 100"
+  }
+
+  depends_on = [
+    local_file.inventory
+  ]
+}
+
+resource "null_resource" "nginx" {
+  provisioner "local-exec" {
+    command = "ANSIBLE_FORCE_COLOR=1 ansible all -i ../ansible/inventory.ini -m ping"
+  }
+
+
+  depends_on = [
+    null_resource.wait
+  ]
+}
