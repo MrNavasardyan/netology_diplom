46ad9a0ae379098d9ad61a444111359d5cf0fd72
ngrachik <ngrachik2011@gmail.com>
2022-07-18 17:09:18 +0300

    Changes to be committed: 	modified:   terraform/gitlab.tf

--- terraform/gitlab.tf
+++ terraform/gitlab.tf
@@ -3,8 +3,8 @@ resource "yandex_compute_instance" "gitlab" {
  hostname ="gitlab.${var.my_domain}"
 
  resources {
-       cores = "4"
-       memory = "4"
+       cores = "2"
+       memory = "2"
  }
 
  boot_disk {
