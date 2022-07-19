ff560ba3f5e246d4d3caef73934cba72c89456b3
ngrachik <ngrachik2011@gmail.com>
2022-07-19 17:21:37 +0300

    Changes to be committed: 	modified:   terraform/ansible.tf

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -8,20 +8,22 @@ resource "null_resource" "wait" {
   ]
 }
 
-
-
 resource "null_resource" "ping" {
   provisioner "local-exec" {
     command = "ANSIBLE_FORCE_COLOR=1 ansible -m ping -i ../ansible/inventory.ini nodes -u centos"
   }
+
+    depends_on = [
+    local_file.wait
+  ]
 }
 
 resource "null_resource" "nginx" {
   provisioner "local-exec" {
-    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook ./ansible/nginx.yml -u centos"
+    command = "ANSIBLE_FORCE_COLOR=1 ansible-playbook ./ansible/roles/nginx/nginx.yml -u centos"
   }
 
   depends_on = [
-    null_resource.wait
+    null_resource.ping
   ]
 }
