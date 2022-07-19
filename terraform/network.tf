c40b464f26ed13394601c8e24d795f1191611260
ngrachik <ngrachik2011@gmail.com>
2022-07-19 14:36:54 +0300

    Changes to be committed: 	modified:   terraform/network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,15 +1,3 @@
-# Network
-# resource "yandex_vpc_network" "network" {
-#   name = "net"
-# }
-
-# resource "yandex_vpc_subnet" "subnet" {
-#   name = "subnet"
-#   zone           = "ru-central1-a-${terraform.workspace}"
-#   network_id     = yandex_vpc_network.subnet.id
-#   v4_cidr_blocks = ["192.168.101.0/24"]
-# }
-
 resource "yandex_vpc_network" "network" {
   name = "network-${terraform.workspace}"
 }
@@ -26,4 +14,33 @@ resource "yandex_vpc_subnet" "mysubnet_2" {
   zone           = "ru-central1-b"
   network_id     = yandex_vpc_network.network.id
   v4_cidr_blocks = ["192.168.11.0/24"]
+}
+
+resource "yandex_dns_zone" "zone1" {
+  name        = "my-private-zone"
+  description = "desc"
+
+  labels = {
+    label1 = "label-1-value"
+  }
+
+  zone             = "grachikn.ru"
+  public           = true
+  private_networks = [yandex_vpc_network.network.id]
+}
+
+resource "yandex_dns_recordset" "rs1" {
+  zone_id = yandex_dns_zone.zone1.id
+  name    = "http://www.grachikn.ru."
+  type    = "A"
+  ttl     = 200
+  data    = ["${yandex_compute_instance.my-domain.network_interface.0.nat_ip_address}"]
+}
+
+resource "yandex_dns_recordset" "rs2" {
+  zone_id = yandex_dns_zone.zone2.id
+  name    = "http://gitlab.grachikn.ru."
+  type    = "A"
+  ttl     = 200
+  data    = ["${yandex_compute_instance.gitlab.network_interface.0.nat_ip_address}"]
 }
