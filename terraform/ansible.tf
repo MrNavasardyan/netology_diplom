9aadadbaa5d78d72c79e456b3af1e7acb4e5152b
ngrachik <ngrachik2011@gmail.com>
2022-07-19 17:56:14 +0300

    Changes to be committed: 	modified:   terraform/ansible.tf

--- terraform/ansible.tf
+++ terraform/ansible.tf
@@ -10,7 +10,7 @@ resource "null_resource" "wait" {
 
 resource "null_resource" "ping" {
   provisioner "local-exec" {
-    command = "ANSIBLE_FORCE_COLOR=1 ansible -m ping -i ../ansible/inventory.ini nodes -u centos"
+    command = "ANSIBLE_FORCE_COLOR=1 ansible -m ping nodes -u centos"
   }
 
   depends_on = [
