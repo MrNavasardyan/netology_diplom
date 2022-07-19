9cc825b68c27d255dcb088937a2a1e6b21c6316a
ngrachik <ngrachik2011@gmail.com>
2022-07-19 14:41:32 +0300

    Changes to be committed: 	modified:   terraform/network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -24,7 +24,7 @@ resource "yandex_dns_zone" "zone1" {
     label1 = "label-1-value"
   }
 
-  zone             = "grachikn.ru"
+  zone             = "grachikn.ru."
   public           = true
   private_networks = [yandex_vpc_network.network.id]
 }
@@ -38,7 +38,7 @@ resource "yandex_dns_recordset" "rs1" {
 }
 
 resource "yandex_dns_recordset" "rs2" {
-  zone_id = yandex_dns_zone.zone2.id
+  zone_id = yandex_dns_zone.zone1.id
   name    = "http://gitlab.grachikn.ru."
   type    = "A"
   ttl     = 200
