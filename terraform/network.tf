c5a7454aeff43ff779c6fcb376a83cb8b85078ad
ngrachik <ngrachik2011@gmail.com>
2022-07-19 14:46:58 +0300

    Changes to be committed: 	modified:   terraform/network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -31,16 +31,8 @@ resource "yandex_dns_zone" "zone1" {
 
 resource "yandex_dns_recordset" "rs1" {
   zone_id = yandex_dns_zone.zone1.id
-  name    = "http://www.grachikn.ru."
+  name    = "grachikn.ru."
   type    = "A"
   ttl     = 200
   data    = ["${yandex_compute_instance.my-domain.network_interface.0.nat_ip_address}"]
-}
-
-resource "yandex_dns_recordset" "rs2" {
-  zone_id = yandex_dns_zone.zone1.id
-  name    = "http://gitlab.grachikn.ru."
-  type    = "A"
-  ttl     = 200
-  data    = ["${yandex_compute_instance.gitlab.network_interface.0.nat_ip_address}"]
 }
